64e4c5c412fe68ce491fdf1d80bae69f42c1c6d4
Mikael <mikael.palmeira.111@gmail.com>
2026-03-09 21:12:39 -0300

    Initial commit - SUS Analytics API

---
+++ src/main/java/br/com/susanalytics/SusAnalyticsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.susanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusAnalyticsApiApplication {
+
+	public static void main(String[] args) { 
+		SpringApplication.run(SusAnalyticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/susanalytics/SusAnalyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.susanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusAnalyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
